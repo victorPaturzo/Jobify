75eb5ff7fb2760271af40dab3caa4f43896e8fe1
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2022-03-25 20:02:02 -0500

    Finished landing page, to include styles using normalize-css styled components. (VPP)

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,8 @@
 import Landing from "./pages/Landing"
-import styled from 'styled-components'
-
-const Button = styled.button`
-background: red;
-color: white;
-font-size: 1 rem;
-`
-
-const ButtonSecond = styled.button`
-background: blue;
-color: white;
-font-size: 1 rem;
-`
 
 function App() {
   return (
     <div>
-      <Button>Click ME</Button>
-      <ButtonSecond>Click ME</ButtonSecond>
-      <h1>jobify</h1>
       <Landing />
     </div>
   )
