0aefda5d485bf63e576b40fa1f1b8464aa3b8ee2
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2022-04-01 19:05:22 -0500

    Started to code more logic into the register page.  Imports include useState, useEffect, Logo, and Wrapper.  Initial state has been set.  handleChange and onSubmit created.  Wrapper has been created in the return and when user submits form the form populates on browser console. (VPP)

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,49 @@
+import { useState, useEffect } from 'react'
+import { Logo } from '../components'
+import Wrapper from '../assets/wrappers/RegisterPage'
+
+const initialState = {
+    name:'',
+    email:'',
+    password:'',
+    isMember: true,
+}
+
 const Register = () => {
+    const [values,setvalues] = useState(initialState)
+    // global state and useNavigate
+
+    const handleChange = (e) => {
+        console.log(e.target)
+    }
+    const onSubmit = (e) => {
+        e.preventDefault()
+        console.log(e.target)
+    }
+
   return (
-    <h1>Register</h1>
+    <Wrapper className='full-page'>
+        <form className='form' onSubmit={onSubmit}>
+            <Logo/>
+            <h3>Login</h3>
+            {/* name input*/}
+            <div className='form-row'>
+                <label htmlFor="name" className='form-label'>
+                    name
+                </label>
+                <input 
+                type="text" 
+                value={values.name} 
+                name="name" 
+                onChange={handleChange} 
+                className='form-input' 
+                />
+            </div>
+            <button type='submit' className="btn btn-block">
+                submit
+            </button>
+        </form>
+    </Wrapper>
   )
 }
 export default Register
