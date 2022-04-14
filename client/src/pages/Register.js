2bb1b8e207da15bf7c6cf4dccd62409a64d275ca
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2022-04-14 14:56:40 -0500

    Created a toggleMember subcomponent for registration page that starts with a login attempt and a message that links to a registration form instead. (VPP)

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -14,6 +14,10 @@ const Register = () => {
     const [values,setvalues] = useState(initialState)
     // global state and useNavigate
 
+    const toggleMember = () => {
+        setvalues({...values, isMember: !values.isMember})
+    }
+
     const handleChange = (e) => {
         console.log(e.target)
     }
@@ -26,15 +30,18 @@ const Register = () => {
     <Wrapper className='full-page'>
         <form className='form' onSubmit={onSubmit}>
             <Logo/>
-            <h3>Login</h3>
+            <h3>{values.isMember ? "Login": 'Register'}</h3>
             {values.showAlert && <Alert />}
             {/* name input*/}
-            <FormRow 
-            type="text" 
-            name="name" 
-            value={values.name} 
-            handleChange={handleChange}
-            />
+            {!values.isMember && (
+                <FormRow 
+                    type="text" 
+                    name="name" 
+                    value={values.name} 
+                    handleChange={handleChange}
+                />
+            )}
+            
             {/* email input*/}
             <FormRow 
             type="email" 
@@ -52,6 +59,13 @@ const Register = () => {
             <button type='submit' className="btn btn-block">
                 submit
             </button>
+            <p>
+                {values.isMember?'Not a member yet?':'Already a member?'}
+                <button type="button" onClick={toggleMember}
+                className='member-btn'>
+                    {values.isMember ? 'Register' : 'Login'}
+                </button>
+            </p>
         </form>
     </Wrapper>
   )
