06e6693601f609c9ce0688b8418ce11960bcc8c4
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2022-04-27 19:19:13 -0500

    Implemented email validator to User schema and successfully tested on Postman. (VPP)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,13 @@
+import User from '../models/User.js'
+
+
 const register = async (req, res) => {
-    res.send('register user')
+    try {
+        const user = await User.create(req.body)
+        res.status(201).json({user})
+    } catch (error) {
+        res.status(500).json({msg: 'There was an error'})
+    }
 }
 const login = async (req, res) => {
     res.send('login user')
