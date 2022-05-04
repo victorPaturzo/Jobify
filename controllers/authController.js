60c2ee6db5c690b7f714ffab35e2aa04f8a7fa7b
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2022-05-04 17:26:54 -0500

    Installed 'express-async-errors' and imported into server.js.  This will allow for the absence of 'try-catch' statements. (VPP)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,9 @@
 import User from '../models/User.js'
 
 
-const register = async (req, res, next) => {
-    try {
+const register = async (req, res) => {
         const user = await User.create(req.body)
         res.status(201).json({user})
-    } catch (error) {
-        next(error)
-    }
 }
 const login = async (req, res) => {
     res.send('login user')
